ba7c9889e5f2897591c32fbdff7b559b9f314808
bitpusher <sky.sanders@gmail.com>
2011-12-03 01:26:29 -0700

    checking for badly formed xml comments

--- src/CIAPI.MetadataGenerator.Tests/GeneratorTests.cs
+++ src/CIAPI.MetadataGenerator.Tests/GeneratorTests.cs
@@ -1,45 +1,37 @@
 using System;
 using System.IO;
+using MetadataGeneration.Core;
 using MetadataGeneration.Core.JsonSchemaDTO;
 using MetadataGeneration.Core.Streaming;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
 
-
-namespace MetadataGeneration.Core.Tests
+namespace CIAPI.MetadataGenerator.Tests
 {
     [TestFixture]
     public class GeneratorTests : GeneratorTestsBase
     {
-        private string _patchJson;
-        private string _smdPatchPath;
         private string _streamingJson;
         private string _validSMD;
         private string _validJsonSchema;
+        private XmlDocSource _xmlDocSource;
 
-        public GeneratorTests()
+        [TestFixtureSetUp]
+        public void SetupTestData()
         {
-            //.\TestData\valid\RESTWebServices.20111129\
-            //_validJsonSchema = File.ReadAllText(@"TestData\valid\RESTWebServices.20111129\Metadata\schema.json");
-            //_validSMD = File.ReadAllText(@"TestData\valid\RESTWebServices.20111129\Metadata\smd.json");
+            _validJsonSchema = File.ReadAllText(@"TestData\valid\Metadata\schema.json.js");
+            _validSMD = File.ReadAllText(@"TestData\valid\Metadata\smd.json.js");
+            _streamingJson = File.ReadAllText(@"TestData\valid\Metadata\streaming.fragment.json");
+            _dtoAssemblyBasePath = @".\TestData\valid\";
+            _xmlDocSource = _wcfConfigReader.Read(_dtoAssemblyBasePath + @"\Web.Config", _dtoAssemblyBasePath);
         }
 
-        private XmlDocSource SetupValidXmlDocSource()
-        {
-            _dtoAssemblyBasePath = @".\TestData\valid\RESTWebServices.20111129-3\";
-            return _wcfConfigReader.Read(_dtoAssemblyBasePath + @"\Web.Config", _dtoAssemblyBasePath);
-        }
         [Test]
         public void StreamingReturnTypesShouldBePresent()
         {
-            var ssmdText = File.ReadAllText(@"TestData\valid\RESTWebServices.20111129-3\Metadata\streaming.fragment.json");
-            JObject smd = JObject.Parse(ssmdText);
-            var xmlDocSource = SetupValidXmlDocSource();
-            
-            MetadataGenerationResult results = _generator.GenerateJsonSchema(xmlDocSource);
-  
-
+            var smd = JObject.Parse(_streamingJson);
+            var results = _generator.GenerateJsonSchema(_xmlDocSource);
+   
             var validator = new StreamingValidator();
             try
             {
@@ -47,42 +39,41 @@ namespace MetadataGeneration.Core.Tests
             }
             catch (MetadataValidationException ex)
             {
-                
                 Assert.Fail(ex.ToString());
             }
-
-            
         }
         [Test]
         public void ValidXmlShouldGenerateValidJsonSchema()
         {
-            var xmlDocSource = SetupValidXmlDocSource();
+            //Checks that dto all have valid XML comments
+            XmlDocUtils.EnsureXmlDocsAreValid(_xmlDocSource);
+            var results = _generator.GenerateJsonSchema(_xmlDocSource);
 
-            MetadataGenerationResult results = _generator.GenerateJsonSchema(xmlDocSource);
-
-            string resultsToString = results.ToString();
+            var resultsToString = results.ToString();
             Assert.IsFalse(results.HasErrors, string.Format("Json Schema generation should not have failed\n\n{0}", resultsToString));
 
-            //TODO: Add this back when we have valid metadata
-//            File.WriteAllText("Generated.Schema.json", results.JsonSchema.ToString());
-//            Assert.AreEqual(_validJsonSchema, results.JsonSchema.ToString());
+            string schema = results.JsonSchema.ToString();
+            File.WriteAllText("Generated.Schema.json", schema);
+            Assert.AreEqual(_validJsonSchema, schema);
         }
 
         [Test]
         public void ValidXmlShouldGenerateValidSMD()
         {
-            var xmlDocSource = SetupValidXmlDocSource();
-            var jsonSchemaResults = _generator.GenerateJsonSchema(xmlDocSource);
-            var smdResults = _generator.GenerateSmd(xmlDocSource, jsonSchemaResults.JsonSchema);
-
+            
+            var jsonSchemaResults = _generator.GenerateJsonSchema(_xmlDocSource);
+            var smdResults = _generator.GenerateSmd(_xmlDocSource, jsonSchemaResults.JsonSchema);
+            _generator.AddStreamingSMD(smdResults.SMD, _streamingJson);
+            
             jsonSchemaResults.MetadataGenerationErrors.ForEach(e => Console.WriteLine(e.ToString()));
-            Assert.That(jsonSchemaResults.HasErrors,Is.False,"SMD generation should not have failed");
+            Assert.That(jsonSchemaResults.HasErrors, Is.False, "SMD generation should not have failed");
             smdResults.MetadataGenerationErrors.ForEach(e => Console.WriteLine(e.ToString()));
-            Assert.That(smdResults.HasErrors,Is.False,"SMD generation should not have failed");
+            Assert.That(smdResults.HasErrors, Is.False, "SMD generation should not have failed");
 
-            //TODO: Add this back when we have valid metadata
-//            File.WriteAllText("Generated.SMD.json", smdResults.SMD.ToString());
-//            Assert.AreEqual(_validSMD, smdResults.SMD.ToString());
+            var smd = smdResults.SMD.ToString();
+
+            File.WriteAllText("Generated.SMD.json", smd);
+            Assert.AreEqual(_validSMD, smd);
         }
     }
 }
